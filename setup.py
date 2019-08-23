10af5ea4131e85bb271a368a1de2177fd19e16e9
Anthony Underwood <au3@sanger.ac.uk>
2019-08-23 11:34:05 +0100

    update version
    Former-commit-id: 2b3ab7fbab3a9134cd08330aca7ef9b584005a9b

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='BactInspectorMax',
-    version='0.0.6',
+    version='0.0.7',
     description='Package to investigate mash hits against refseq',
     author='Anthony Underwood',
     author_email='au3@sanger.ac.uk',
